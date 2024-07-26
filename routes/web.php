865f43932ca215132a44fd48da57c25b3304b5e5
u0222 <ootayy411@gmail.com>
2024-07-27 08:14:06 +0900

    図書管理システムの実装  完成

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::get('library/index', [LibraryController::class, 'index']);
     Route::get('library/borrow/{id}', [LibraryController::class, 'borrowingForm']);
     Route::post('library/borrow', [LibraryController::class, 'borrow']);
+    Route::post('library/return', [LibraryController::class, 'returnBook']);
+    Route::get('library/history', [LibraryController::class, 'history']);
 
 });
 
